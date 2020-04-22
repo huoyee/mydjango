d5ee62abda77b28f0351acfa089f65f2f852c0d3
chenliang <306125304@qq.com>
2020-04-22 12:39:37 +0800

    linux 启动命令

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 conda activate mydjango
-cd /home/githup/mydjango
+cd /home/github/mydjango/
 
 #kill
-ps -ef | grep 'mydjango' | grep 'manage.py' |grep -v grep |cut -c 9-15| xargs kill -9
-ps -ef | grep 'manage.py' | grep 'beat' |grep -v grep |cut -c 9-15 | xargs kill -9
-ps -ef | grep 'manage.py' | grep 'worker' |grep -v grep | cut -c 9-15 | xargs kill -9
-ps -ef | grep 'flower' |grep -v grep | cut -c 9-15 | xargs kill -9
+#ps -ef | grep 'mydjango' | grep 'manage.py' |grep -v grep |cut -c 9-15| xargs kill -9
+#ps -ef | grep 'manage.py' | grep 'beat' |grep -v grep |cut -c 9-15 | xargs kill -9
+#ps -ef | grep 'manage.py' | grep 'worker' |grep -v grep | cut -c 9-15 | xargs kill -9
+#ps -ef | grep 'flower' |grep -v grep | cut -c 9-15 | xargs kill -9
 
 #run
 nohup python manage.py runserver 139.129.92.179:5000 &

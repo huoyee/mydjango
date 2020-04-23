a21558e535a5ae6ae2da3c0e3c645f217e5ffa90
chenliang <306125304@qq.com>
2020-04-23 17:58:38 +0800

    准备使用supervisor管理进程

--- stop.sh
+++
@@ -1,4 +0,0 @@
-ps -ef | grep 'mydjango' | grep 'manage.py' |cut -c 9-15| xargs kill -9
-ps -ef | grep 'manage.py' | grep 'beat' |cut -c 9-15 | xargs kill -9
-ps -ef | grep 'manage.py' | grep 'worker' | cut -c 9-15 | xargs kill -9
-ps -ef | grep 'flower' |grep -v grep | cut -c 9-15 | xargs kill -9
